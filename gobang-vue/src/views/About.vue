4360dabc5f6e0d67b5dc10c795b0c9b6b2e8f9dd
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-05-17 18:50:34 +0800

    update config

--- gobang-vue/src/views/About.vue
+++ gobang-vue/src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
     <el-form>
-        <el-form-item :label="$t('lang.about.author')">Joshua Wang</el-form-item>
-        <el-form-item :label="$t('lang.about.email')">urfriendmyfriend@163.com</el-form-item>
-        <el-form-item :label="$t('lang.about.github')">https://github.com/AnizOoalGown</el-form-item>
+        <el-form-item :label="$t('lang.about.author')">Chunsheng Zhang</el-form-item>
+        <el-form-item :label="$t('lang.about.email')">zhangcs_423@163.com</el-form-item>
+        <el-form-item :label="$t('lang.about.github')">https://github.com/zhangchunsheng</el-form-item>
     </el-form>
 </template>
 
